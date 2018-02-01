d92f53f0c8f76dd06cf6007a072b97c2b59a2af7
Nicolas Paes Leme <nicolaspleme@gmail.com>
2018-02-01 01:01:15 -0200

    Spawn Arena Added
    - SpawnArena.cs takes one prefab (gameObject) and creates 9 copies of it next to eachother in a 3x3 matrix form
    - Created TestTile to be spawned for now
    - Created tileTestMaterial to be able to see the player cube better

---
+++ PingPong/Assets/SpawnArena.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArena : MonoBehaviour {
+
+	public GameObject Tile1;
+	private int Columns;
+	private int Rows = -1;
+	private float tileSize;
+
+	
+	void Start () 
+	{
+		tileSize = Tile1.transform.localScale.x;
+
+		for(int i=0;i<9;i++)
+		{
+			Columns++;
+			if(i%3 == 0)
+			{
+				Columns = 0;
+				Rows++;
+			}			
+			Instantiate(Tile1,new Vector3 (10*tileSize*Columns,-0.5f,10*tileSize*Rows),Quaternion.identity,this.gameObject.transform);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
