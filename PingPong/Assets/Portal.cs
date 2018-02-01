94846c9c86e489fceb42126979f1047d83e57f14
Nicolas Paes Leme <nicolaspleme@gmail.com>
2018-02-01 19:34:35 -0200

    Portal and finally the movement fix
    - Never go full diagonal
    - Portal is dumb and expect to be pleased with a player collision

---
+++ PingPong/Assets/Portal.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour 
+{
+	public bool activate = true;
+	public int playerWin;
+	
+	  void OnCollisionEnter(Collision col)
+	{
+		if(col.collider.gameObject.tag == "Player" && activate)
+		{
+			playerWin++;
+			col.collider.gameObject.SetActive(false);
+		}
+	}
+}
