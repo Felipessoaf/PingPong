9957aa3db5a948bd90fc7189c6193d4d72d4469c
Nicolas Paes Leme <nicolaspleme@gmail.com>
2018-02-01 17:10:33 -0200

    Fixed 30o for Main Camera

--- PingPong/Assets/Scripts/CameraFollow.cs
+++ PingPong/Assets/Scripts/CameraFollow.cs
@@ -19,6 +19,7 @@ public class CameraFollow : MonoBehaviour {
 				Vector3 desiredPosition = target.position + offset;
 				Vector3 smoothedPosition = Vector3.Lerp(transform.position,desiredPosition,smoothSpeed);
 				transform.position = smoothedPosition;
+				transform.rotation = Quaternion.Euler(30,0,0);
 				//transform.LookAt(target);
 			}
 	}
