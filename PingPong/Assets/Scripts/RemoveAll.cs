88a631d664ae4295a13206857bc090c939fb529c
Felipe Pessoa <felipessoaf@gmail.com>
2018-02-01 17:13:39 -0200

    death anim end match

---
+++ PingPong/Assets/Scripts/RemoveAll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveAll : Photon.MonoBehaviour {
+    
+
+    public void EndAnim()
+    {
+        foreach (GameObject o in GameObject.FindGameObjectsWithTag("Monster"))
+        {
+            PhotonView v = PhotonView.Get(o);
+            v.RPC("end", PhotonTargets.All);
+        }
+        foreach (GameObject o in GameObject.FindGameObjectsWithTag("Hero"))
+        {
+            PhotonView v = PhotonView.Get(o);
+            v.RPC("end", PhotonTargets.All);
+        }
+    }
+}
