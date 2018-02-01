501d9dfac13149d68bf57f23e92d92c6fb45cb4c
Nicolas Paes Leme <nicolaspleme@gmail.com>
2018-02-01 00:28:16 -0200

    Movement.cs started
    - Basic Movement Added
    - PlayerVelocity GD variable added

---
+++ PingPong/Assets/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour 
+{
+	[Range(1.0f,50f)]
+	public float playerVelocity;
+    public Vector3 playerVelocityVector;
+
+	Rigidbody playerRb;
+
+	void Awake()
+	{
+		playerRb = GetComponent<Rigidbody>();
+	}
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		playerVelocityVector.x = Input.GetAxis("Horizontal");
+		playerVelocityVector.z = Input.GetAxis("Vertical");
+
+		if(Input.anyKey)
+		{
+			playerRb.velocity = playerVelocity * playerVelocityVector; //new Vector3 (0,0,playerVelocity);
+			//Debug.Log("W");
+		}
+	}
+}
