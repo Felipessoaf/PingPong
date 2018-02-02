13a64ea9ea673b9e5ada4955005954cf6abe5134
Bruna Lucchesi <brunalvillela@gmail.com>
2018-02-02 11:45:37 -0200

    add song to main scene + menu and credits scenes

---
+++ PingPong/Assets/Scripts/LoadSceneScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneScript : MonoBehaviour {
+	public void LoadScene (string name) {
+		if (name == "quit") {
+			Application.Quit ();
+		} else {
+			SceneManager.LoadScene (name);
+		}
+	}
+}
