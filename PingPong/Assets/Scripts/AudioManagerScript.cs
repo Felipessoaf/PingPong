ef179c26fe42a0a59a8b78fd51f03f69f48371ab
Felipe Pessoa <felipessoaf@gmail.com>
2018-02-02 11:48:34 -0200

    audio manager

---
+++ PingPong/Assets/Scripts/AudioManagerScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class AudioManagerScript : MonoBehaviour
+    {
+        private static AudioManagerScript _audioManager;
+        private void Awake()
+        {
+            if (!_audioManager)
+            {
+                _audioManager = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+            //    Debug.Log("alo");
+                 Destroy(this.gameObject);
+            }
+        }
+
+        // public void DestroyOnLoad()
+        // {
+        //     Destroy(this.gameObject);
+        // }
+
+        void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        // called second
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if(scene.name == "win" || scene.name =="menu"){
+                GetComponent<AudioSource>().mute = true;
+            }
+            else{
+                GetComponent<AudioSource>().mute = false;
+
+                //if(menuwin)GetComponent<AudioSource>().Pause();
+                //else GetComponent<AudioSource>().Play();
+            }
+        }
+
+        // called when the game is terminated
+        void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+}
