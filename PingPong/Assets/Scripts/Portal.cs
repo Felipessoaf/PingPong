1abf4147555c896c60f9f3b0899fa3aa36773600
Nicolas Paes Leme <nicolaspleme@gmail.com>
2018-02-02 01:44:45 -0200

    Portal working, simple stuff

--- PingPong/Assets/Scripts/Portal.cs
+++ PingPong/Assets/Scripts/Portal.cs
@@ -1,16 +1,36 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Portal : MonoBehaviour 
 {
-	public bool activate = true;
-	public int playerWin;
-	
-	  void OnCollisionEnter(Collision col)
-	{
-		if(col.collider.gameObject.tag == "Player" && activate)
+		public bool activate = true;
+		public int playerWin;
+		public ParticleSystem portalParticles;
+
+		void Start()
 		{
-			playerWin++;
-			col.collider.gameObject.SetActive(false);
+			StartCoroutine(activatePortalTime(5f));
 		}
+		
+		void OnCollisionEnter(Collision col)
+		{
+			if(col.collider.gameObject.tag == "Player" && activate)
+			{
+				playerWin++;
+				col.collider.gameObject.SetActive(false);
+			}
+		}
+
+	IEnumerator activatePortalTime(float time)
+	{
+		Debug.Log("Portal time activated");
+		yield return new WaitForSeconds(time);
+		activatePortal();
+	}
+	void activatePortal()
+	{
+		activate = true;
+		if(portalParticles.isStopped)portalParticles.Play();
+		GetComponent<Collider>().enabled = true;
 	}
 }
